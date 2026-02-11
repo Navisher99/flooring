c4d7485e36549fa403322487d20c8c5affc6fdc3
Navisher99 <93440352+Navisher99@users.noreply.github.com>
2026-02-11 16:22:30 +1000

    Update app.js

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ const floors = [
   {
     name: "Timber Flooring",
     price: "From $85 / m²",
-    image: "first.jpg",
+    image: "img/first.jpg",
     description: "High-end timber installation with perfect alignment and finish."
   },
   {
     name: "Laminate Flooring",
     price: "From $45 / m²",
-    image: "second.jpg",
+    image: "img/second.jpg",
     description: "Clean, efficient installation with attention to detail."
   },
   {
     name: "Vinyl Plank Flooring",
     price: "From $55 / m²",
-    image: "third.jpg",
+    image: "img/third.jpg",
     description: "Modern vinyl plank installation, durable and stylish."
   },
   {
     name: "Tile Flooring",
     price: "From $70 / m²",
-    image: "forth.jpg",
+    image: "img/forth.jpg",
     description: "Precision tile installation for wet areas and living spaces."
   }
 ];
@@ -41,4 +41,5 @@ floors.forEach((floor, index) => {
     </div>
   `;
 });
+
 
